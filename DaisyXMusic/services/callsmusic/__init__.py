c7e6955b581305d8f64efdd949a78402b28a4f08
Technical_Hunter <85411336+Technical-Hunter@users.noreply.github.com>
2021-08-12 19:04:44 +0530

    Multiple Assistant

--- DaisyXMusic/services/callsmusic/__init__.py
+++ DaisyXMusic/services/callsmusic/__init__.py
@@ -1,6 +1,11 @@
 from pyrogram import Client
+from pyrogram import idle
 
 from DaisyXMusic import config
 
-client = Client(config.SESSION_NAME, config.API_ID, config.API_HASH)
-run = client.run
+client1 = Client(config.SESSION_NAME1, config.API_ID1, config.API_HASH1)
+client2 = Client(config.SESSION_NAME2, config.API_ID2, config.API_HASH2)
+
+run1 = client1.run1
+run2 = client2.run2
+idle
