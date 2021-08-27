2d000dfaa61365e76ad22219db9b11e93fb61281
Inuka Asith <inukaasith7@gmail.com>
2021-08-27 07:03:39 +0530

    Update __main__.py

--- DaisyXMusic/__main__.py
+++ DaisyXMusic/__main__.py
@@ -1,4 +1,5 @@
 # DaisyXMusic (Telegram bot project)
+
 # Copyright (C) 2021  Inuka Asith & Rojserbest
 
 # This program is free software: you can redistribute it and/or modify
@@ -10,16 +11,17 @@
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU Affero General Public License for more details.
-#
+
 # You should have received a copy of the GNU Affero General Public License
 # along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
 
 import requests
 from pyrogram import Client as Bot
+from pyrogram import idle
 
 from DaisyXMusic.config import API_HASH, API_ID, BG_IMAGE, BOT_TOKEN
-from DaisyXMusic.services.callsmusic import run
+from DaisyXMusic.services.callsmusic.callsmusic import run
 
 response = requests.get(BG_IMAGE)
 file = open("./etc/foreground.png", "wb")
@@ -36,3 +38,4 @@ bot = Bot(
 
 bot.start()
 run()
+idle()
